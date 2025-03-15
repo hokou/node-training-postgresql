ecf37f626931826511f6de1341b56d8baf6014f8
hokou <hokou.shiau@gmail.com>
2025-03-15 22:42:13 +0800

    feat: update course api

--- week5/routes/admin.js
+++ week5/routes/admin.js
@@ -7,6 +7,133 @@ const logger = require('../utils/logger')('Admin')
 const { isValidString, isNumber } = require('../utils/validUtils')
 const e = require('express')
 
+router.post('/coaches/courses', async (req, res, next) => {
+  try {
+    const { user_id, skill_id, name, description, start_at, end_at, max_participants, meeting_url } = req.body
+
+    if(!isValidString(user_id) || !isValidString(skill_id) || !isValidString(name)
+    || !isValidString(description) || !isValidString(start_at) || !isValidString(end_at)
+    || !isNumber(max_participants) || !isValidString(meeting_url) || !meeting_url.startsWith('https')) {
+      res.status(400).json({
+        status : "failed",
+        message: "欄位未填寫正確"
+      })
+      return
+    }
+
+    const userRepo = dataSource.getRepository('User')
+    const findUser = await userRepo.findOne({
+      where: {
+        id: user_id
+      }
+    })
+    if (!findUser) {
+      res.status(400).json({
+        status: 'failed',
+        message: '使用者不存在'
+      })
+      return
+    } else if (findUser.role !== 'coach') {
+      res.status(409).json({
+        status: 'failed',
+        message: '使用者尚未成為教練'
+      })
+      return
+    }
+
+    const courcesRepo = dataSource.getRepository('Course')
+    const newCourse = await courcesRepo.create({
+      user_id,
+      skill_id,
+      name,
+      description,
+      start_at,
+      end_at,
+      max_participants,
+      meeting_url
+    })
+    
+    const result = await courcesRepo.save(newCourse)
+
+    res.status(201).json({
+      status: "success",
+      data: {
+        course: result
+      }
+    })
+  } catch (error) {
+    logger.error(error)
+    next(error)
+  }
+})
+
+router.put('/coaches/courses/:courseId', async (req, res, next) => {
+  try {
+    const { courseId } = req.params
+    const { skill_id, name, description, start_at, end_at, max_participants, meeting_url } = req.body
+
+    if( !isValidString(courseId)
+    ||  !isValidString(skill_id) || !isValidString(name)
+    || !isValidString(description) || !isValidString(start_at) || !isValidString(end_at)
+    || !isNumber(max_participants) || !isValidString(meeting_url) || !meeting_url.startsWith('https')) {
+      res.status(400).json({
+        status : "failed",
+        message: "欄位未填寫正確"
+      })
+      return
+    }
+
+    const courseRepo = dataSource.getRepository('Course')
+    const findCourse = await courseRepo.findOne({
+      where: {
+        id: courseId
+      }
+    })
+    if (!findCourse) {
+      res.status(400).json({
+        status: 'failed',
+        message: '課程不存在'
+      })
+      return
+    }
+
+    const updateCourse = await courseRepo.update({
+      id: courseId
+    }, {
+      skill_id,
+      name,
+      description,
+      start_at,
+      end_at,
+      max_participants,
+      meeting_url
+    })
+
+    if (updateCourse.affected === 0) {
+      res.status(400).json({
+        status: 'failed',
+        message: '更新課程失敗'
+      })
+      return
+    }
+
+    const courseResult = await courseRepo.findOne({
+      where: {
+        id: courseId
+      }
+    })
+
+    res.status(201).json({
+      status: "success",
+      data: {
+        course: courseResult
+      }
+    })
+  } catch (error) {
+    logger.error(error)
+    next(error)
+  }
+})
 
 router.post('/coaches/:userId', async (req, res, next) => {
   try {
