61e6dbad55fb2e7a03d3d8ed1f2fd046d25b0dda
hokou <hokou.shiau@gmail.com>
2025-03-15 22:56:01 +0800

    feat: add wee6

---
+++ week6/routes/creditPackage.js
@@ -0,0 +1,95 @@
+const express = require('express')
+
+const router = express.Router()
+const { dataSource } = require('../db/data-source')
+const logger = require('../utils/logger')('CreditPackage')
+const { isValidString, isNumber } = require('../utils/validUtils')
+
+router.get('/', async (req, res, next) => {
+  try {
+    const data = await dataSource.getRepository("CreditPackage").find({
+      select: ["id", "name", "credit_amount", "price"]
+    })
+    res.status(200).json({
+      status: "success",
+      data: data
+    })
+  } catch (error) {
+    next(error)
+  }
+})
+
+router.post('/', async (req, res, next) => {
+  try {
+    const {name, credit_amount, price} = req.body;
+    
+    if (!isValidString(name) || !isNumber(credit_amount) || !isNumber(price)) {
+      res.status(400).json({
+        status: "failed",
+        message: "欄位未填寫正確",
+      })
+      return
+    }
+
+    const creditPackage = dataSource.getRepository("CreditPackage")
+    const isExist = await creditPackage.findOne({
+      where: {
+        name: name
+      }
+    })
+    if (isExist) {
+      res.status(409).json({
+        status: "failed",
+        message: "資料重複",
+      })
+      return
+    }
+
+    const newCreditPackage = creditPackage.create({
+      name,
+      credit_amount,
+      price
+    })
+
+    const result = await creditPackage.save(newCreditPackage)
+
+    res.status(200).json({
+      status: "success",
+      data: result
+    })
+
+  } catch (error) {
+    next(error)
+  }
+})
+
+router.delete('/:creditPackageId', async (req, res, next) => {
+  try {
+    const { creditPackageId } = req.params;
+
+    if (!isValidString(creditPackageId)) {
+      res.status(400).json({
+        status: "failed",
+        message: "ID錯誤",
+      })
+      return
+    }
+
+    const result = await dataSource.getRepository("CreditPackage").delete(creditPackageId);
+    if (result.affected === 0) {
+      res.status(400).json({
+        status: "failed",
+        message: "ID錯誤",
+      })
+      return
+    }
+    res.status(200).json({
+      status: "success",
+    })
+
+  } catch (error) {
+    next(error)
+  }
+})
+
+module.exports = router
---
+++ week6/routes/skill.js
@@ -0,0 +1,93 @@
+const express = require('express')
+
+const router = express.Router()
+const { dataSource } = require('../db/data-source')
+const logger = require('../utils/logger')('skill')
+const { isValidString, isNumber } = require('../utils/validUtils')
+
+router.get('/', async (req, res, next) => {
+  try {
+    const data = await dataSource.getRepository("Skill").find({
+      select: ["id", "name"]
+    })
+    res.status(200).json({
+      status: "success",
+      data: data
+    })
+  } catch (error) {
+    next(error)
+  }
+})
+
+router.post('/', async (req, res, next) => {
+  try {
+    const { name } = req.body;
+    
+    if (!isValidString(name)) {
+      res.status(400).json({
+        status: "failed",
+        message: "欄位未填寫正確",
+      })
+      return
+    }
+
+    const skillRepo = dataSource.getRepository("Skill")
+    const isExist = await skillRepo.findOne({
+      where: {
+        name: name
+      }
+    })
+    if (isExist) {
+      res.status(409).json({
+        status: "failed",
+        message: "資料重複",
+      })
+      return
+    }
+
+    const newSkill = skillRepo.create({
+      name
+    })
+
+    const result = await skillRepo.save(newSkill)
+
+    res.status(200).json({
+      status: "success",
+      data: result
+    })
+
+  } catch (error) {
+    next(error)
+  }
+})
+
+router.delete('/:skillId', async (req, res, next) => {
+  try {
+    const { skillId } = req.params;
+
+    if (!isValidString(skillId)) {
+      res.status(400).json({
+        status: "failed",
+        message: "ID錯誤",
+      })
+      return
+    }
+
+    const result = await dataSource.getRepository("Skill").delete(skillId);
+    if (result.affected === 0) {
+      res.status(400).json({
+        status: "failed",
+        message: "ID錯誤",
+      })
+      return
+    }
+    res.status(200).json({
+      status: "success",
+    })
+
+  } catch (error) {
+    next(error)
+  }
+})
+
+module.exports = router
