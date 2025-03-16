9d9e82f7619ea009cd90fe123640a4f02c51b60a
hokou <hokou.shiau@gmail.com>
2025-03-16 08:43:56 +0800

    feat: update JWT verify

--- week6/utils/jwtUtils.js
+++ week6/utils/jwtUtils.js
@@ -13,6 +13,27 @@ const generateJWT = (payload)=> {
   );
 }
 
+const verifyJWT = (token) => {
+  return new Promise((resolve, reject) => {
+    jwt.verify(token, config.get('secret.jwtSecret'), (err, decoded) => {
+      if (err) {
+        // reject(err)
+        switch (err.name) {
+          case 'TokenExpiredError':
+            reject(appError(401, 'Token 已過期'))
+            break
+          default:
+            reject(appError(401, '無效的 token'))
+            break
+        }
+      } else {
+        resolve(decoded)
+      }
+    })
+  })
+}
+
 module.exports = { 
   generateJWT,
+  verifyJWT
 };
